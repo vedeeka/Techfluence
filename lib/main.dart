4bd651c0357c996a199341d7cc2b310a0fe29d35
annkurrrr <ankur.kunde@gmail.com>
2025-03-28 02:37:44 +0530

    Ankur work

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:techfluence/pages/depreciation.dart';
 import 'component/dashboard components/homepage.dart';
 
 void main() async {
